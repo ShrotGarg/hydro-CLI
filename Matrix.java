7e8a03358fd61cfd295247bc4fda665a9729e9dc
ShrotGarg <Shrotgarg1@gmail.com>
2026-05-11 17:17:03 +0530

    Added matrix rain effect :)

--- Matrix.java
+++ Matrix.java
@@ -10,7 +10,7 @@ public class Matrix implements commands{
     //creates the matrix rain
     public void execute() throws InterruptedException{
         int width = 100;
-        int height = 50;
+        int height = 24;
         int len = 10;
 
         int yPos[] = new int[width];
@@ -20,8 +20,8 @@ public class Matrix implements commands{
         Random rand = new Random();
 
         for(int i = 0; i < width; i++){
-            yPos[i] = rand.nextInt(height);
-            speed[i] = rand.nextInt(10);
+            yPos[i] = rand.nextInt(height) + 1;
+            speed[i] = rand.nextInt(5) + 1;
             count[i] = 0;
         }
 
@@ -30,39 +30,45 @@ public class Matrix implements commands{
 
         while(true){
             for(int i = 0; i < yPos.length; i++){
-                int y = yPos[i];
-                int x = i + 1;
-
                 if(count[i] >= speed[i]){
+                    int y = yPos[i];
+                    int x = i + 1;
 
                     //clean the trail
                     int clean = y - len;
-                    if(clean >= 0 && clean <= height)
-                        System.out.printf("\033[%d;%dH ", y, x);
+                    if(clean > 0 && clean <= height)
+                        System.out.printf("\033[%d;%dH ", clean, x);
 
                     //print green char
-                    if(y >= 0 && y <= height){
+                    if(y > 0 && y <= height){
                         System.out.printf("\033[%d;%dH", y, x);
-                        System.out.printf("\033[32m%c", rand.nextInt(26) + 'a');
+                        System.out.printf("\033[0;32m%c", rand.nextInt(26) + 'a');
                     }
 
+                    int ny = y + 1;
                     //print white char
-                    x++;
-                    if(y >= 0 && y <= height){
-                        System.out.printf("\033[%d;%dH", y, x);
+                    if(ny > 0 && ny <= height){
+                        System.out.printf("\033[%d;%dH", ny, x);
                         System.out.printf("\033[1;37m%c", rand.nextInt(26) + 'a');
                     }
 
+                    yPos[i]++;
+                    count[i] = 0;
+
                     //reset back to 0
-                    if(y - len >= height)
-                        y = 0;
+                    if(yPos[i] - len > height){
+                        yPos[i] = 0;
+                        speed[i] = rand.nextInt(5) + 1;
+                    }
+
+                    count[i] = 0;
                 }
                 else
                     count[i]++;
             }
 
             System.out.flush();
-            Thread.sleep(20);
+            Thread.sleep(50);
         }
     }
 }
--- hydro.java
+++ hydro.java
@@ -1,9 +1,12 @@
 import java.util.HashMap;
 public class hydro{
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
         HashMap<String, commands>  map = new HashMap<>();
 
+        // cmd name, obj of the class
         map.put("time", new displayTime());
+        map.put("matrix", new Matrix());
+
         commands obj = map.get(args[0]);
         obj.execute();
     }
